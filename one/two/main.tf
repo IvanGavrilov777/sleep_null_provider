522777e0f280203581d095d97b94548a9bf28805
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-06-07 14:25:24 +0300

    Create main.tf

---
+++ one/two/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+variable "condition" {
+  default = false
+}
+
+resource "null_resource" "sleep" {
+triggers = {
+time = timestamp ()
+}
+count = var.condition ? 1 : 65000
+  provisioner "local-exec" {
+    command = <<EOT
+    echo 'Hello World'
+    sleep 30
+    EOT
+  }
+}
